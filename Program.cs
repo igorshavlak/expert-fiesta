c8f92b62188895ffb4cda21917fb2892993cae95
igorshavlak <igorshavlak8@gmail.com>
2021-12-15 10:53:50 +0200

    Додано підрахунок суми чисел і виведення результату

--- Program.cs
+++ Program.cs
@@ -6,35 +6,10 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int []arr = new int[n];
-            Random rnd = new Random();
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = rnd.Next(-5, 20);
-                Console.WriteLine(arr[i]);
-            }
-            int index = 0;
-            Console.WriteLine();
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] < 0)
-                {
-                    index = i;
-                }
-                
-            }
-            Console.WriteLine(index);
-            int sum = 0;
-            int arit_mean;
-            for (int i = index; i < arr.Length; i++)
-            {
-                sum = sum + arr[i];
-
-            }
-            Console.WriteLine(sum);
-            arit_mean = (int)(sum / arr.Length);
-            Console.WriteLine(arit_mean);
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5+10={0}",x+y);
+            Console.ReadKey();
         }
     }
 }
